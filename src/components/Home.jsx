450b6cfd1678e1e1167bf6158715326c961a70b2
abolarin100 <abolarin100@gmail.com>
2023-07-10 13:25:06 -0700

    made updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
     return (
 
       <div className='head'>
-            <section className="py-5 text-center container ">
+  <section className=" text-center container ">
     <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto home">
+      <div className="col-lg-6 col-md-8 mx-auto">
         <h1 className="fw-light">Welcome to My Portfolio Website</h1>
-        <p className="home">As a passionate Frontend Developer with two years of experience, I am skilled in creating intuitive, user-friendly interfaces using HTML, CSS, Bootstrap, JavaScript, and React. I can seamlessly integrate REST APIs in React. I have experience collaborating with designers, back-end developers, and project managers to ensure seamless integration of all components. I am also passionate about staying up-to-date with the latest trends and best practices in web development, and regularly attend conferences and workshops to expand my knowledge. I can add immense value with skills and capacities while experiencing personal and professional growth.</p>
+        <div className="home">As a passionate Frontend Developer with two years of experience, I am skilled in creating intuitive, user-friendly interfaces using HTML, CSS, Bootstrap, JavaScript, and React. I can seamlessly integrate REST APIs in React. I have experience collaborating with designers, back-end developers, and project managers to ensure seamless integration of all components. I am also passionate about staying up-to-date with the latest trends and best practices in web development, and regularly attend conferences and workshops to expand my knowledge. I can add immense value with skills and capacities while experiencing personal and professional growth.</div>
         <p>
           <a href="/blog" className="btn btn-secondary my-2">My Projects</a>
         </p>
